4570d451c012fd250ba04feaa0345e55a5a58863
Stjepan Svib <svib.stjepan@gmail.com>
2025-03-25 21:57:08 +0100

    Finished script

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ const gridSizeTextDisplay = document.querySelectorAll("#gridSizeTextDisplay");
 
 
 const gridContainerDisplay = document.querySelector("#gridDisplayContainer");
-const gridDisplay = document.querySelector("#gridDsplay");
+const gridDisplay = document.querySelector("#gridDisplay");
 
+let selectedMode;
+let isDrawing = false;
 
 const createGrid = function(size){
 
@@ -34,21 +36,74 @@ const createGrid = function(size){
 
     boxSize = Math.floor((gridContainerDisplay.clientWidth / size));
 
+    gridContainerDisplay.addEventListener("mousedown", (e)=>{
+        if(e.button == 0){
+            isDrawing = true;
+            e.preventDefault();
+        }
+    });
+
+    document.addEventListener("mouseup", (e)=>{
+        isDrawing = false;
+    })
+
 
     for(let i = 0; i < size * size; i++){
         let box = document.createElement("div");
         box.style.border = "1px solid black";
         box.style.width = boxSize +"px";
         box.style.height = boxSize + "px";
-        box.style.opacity = "0.2";
+        box.style.opacity = "0.8";
 
         gridContainerDisplay.appendChild(box);
-    };
 
+        box.addEventListener("mouseenter", () =>{
+            if(isDrawing){
+
+                if(selectedMode == "colorPicker"){
+                    box.style.backgroundColor = colorPickerInputBnt.value;
+                    }
+                else if(selectedMode == "rainbow"){
+                     box.style.backgroundColor = `rgb(${Math.random()*256}, ${Math.random()* 256}, ${Math.random()* 256})`
+                    }
+                else if(selectedMode == "eraser"){
+                    box.style.backgroundColor = "white";
+                    }
+                else if(selectedMode == "color"){
+                    box.style.backgroundColor = "black";
+                    } 
+            }
+        });
+    };
 }
 
+
+clearBtn.addEventListener("click", function(){
+    const boxes = document.querySelectorAll("div");
+    boxes.forEach((box)=>{
+        box.style.backgroundColor = "white";
+    })
+})
+
+
 gridSizeRangeDisplay.addEventListener("input",function(){
-    let size = parseFloat(this.value, 10);
-    if(size > 1 && size < 64) createGrid(size);
+    let size = (this.value);
+    createGrid(size);
 });
 
+
+colorPickerInputBnt.addEventListener("click", function(){
+    selectedMode = "colorPicker";
+});
+
+rainbowBtn.addEventListener("click", function(){
+    selectedMode = "rainbow";
+})
+
+eraserBtn.addEventListener("click", function(){
+    selectedMode = "eraser";
+});
+
+colorBnt.addEventListener("click", function(){
+    selectedMode = "color";
+});
